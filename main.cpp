68fd97d20d378e93ffcc36a75f2087bd1a455e64
Saiham Islam Abid <siaabid5323@gmail.com>
2021-05-23 08:11:10 +0000

    abid

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "bits/stdc++.h"
 #include "library.h"
 #include "fstream"
+#include "saiham.cpp"
+
 using namespace std;
 
 // const int n=10;
-const char *filename = "data.txt";
-map<string, string> usr_pwd;
+const char *filename = "userdata.txt";
+map<string, pair<string, string>> usr_pwd;
 fstream file;
 
 void play();
@@ -15,18 +17,26 @@ void signup();
 
 int main()
 {
+    system("clear");
+    savedata();
+
     string uuid;
     file.open(filename, ios::in);
     if (file)
     {
         while (file >> uuid)
         {
-            file >> usr_pwd[uuid];
+            file >> usr_pwd[uuid].first >> usr_pwd[uuid].second;
         }
     }
     file.close();
 
-    prompt();
+    for(auto X : usr_pwd)
+    {
+        cout << X.first << " " << X.second.first<<" "<<X.second.second<<endl;
+    }
+
+    // prompt();
     // auto gm = gameboard.MAP;
     // for (int i = 1; i < gm.size(); i++)
     // {
@@ -36,76 +46,6 @@ int main()
     return 0;
 }
 
-void login()
-{
-    string usrname, passwd;
-    cout << "Username: ";
-    cin >> usrname;
-    cout << "Password: ";
-    cin >> passwd;
-    if (usr_pwd[usrname] == passwd)
-    {
-        cout << "Login success!" << endl;
-        play();
-    }
-    else
-    {
-        cout << "Wrong password! Try again" << endl;
-        prompt();
-    }
-}
-
-void signup()
-{
-    string usrname, passwd1, passwd2;
-    int lt;
-    cout << "Username: ";
-    cin >> usrname;
-    if (usr_pwd.find(usrname) != usr_pwd.end())
-    {
-        cout << "Username already exist. press 1 to login or press 2 to try again..." << endl;
-        cin >> lt;
-        lt == 1 ? login() : signup();
-    }
-    cout << "Password: ";
-    cin >> passwd1;
-    cout << "Confirm password: ";
-    cin >> passwd2;
-    if (passwd1 == passwd2)
-    {
-        file.open(filename, ios::app);
-        file << usrname << endl
-             << passwd1 << endl;
-        file.close();
-        cout << "Registered successfully!" << endl;
-        play();
-    }
-    else
-    {
-        cout << "Password doesn't match! Try again..." << endl;
-        signup();
-    }
-}
-
-void prompt()
-{
-    int ch;
-    cout << "Enter your choice (1-login 2-register):";
-    cin >> ch;
-    switch (ch)
-    {
-    case 1:
-        login();
-        break;
-    case 2:
-        signup();
-        break;
-    default:
-        cout << "Invalid Input. Try again..." << endl;
-        prompt();
-        break;
-    }
-}
 
 void play()
 {
--- saiham.cpp
+++ saiham.cpp
@@ -1,17 +1,90 @@
 #include "bits/stdc++.h"
 #include "fstream"
+#include "time.h"
+#include "stdlib.h"
 using namespace std;
 
-int main()
+void savedata()
 {
     fstream file;
-    file.open("in.csv", ios::in);
+    file.open("userdata.txt", ios::out | ios::app);
 
-    string greetings, commit;
+    string name, pass, div;
     
-    file >> greetings >> commit;
-
-    cout<< greetings <<endl<<commit<<endl;
+    cout<<"Username: "; cin>>name; file << name<<" ";
+    cout<<"Password: "; cin>>pass; file << pass<<" ";
+    cout<<"Division: "; cin>>div;  file << div<<endl;
     
-    return 0;
 }
+
+
+// void login()
+// {
+//     string usrname, passwd;
+//     cout << "Username: ";
+//     cin >> usrname;
+//     cout << "Password: ";
+//     cin >> passwd;
+//     if (usr_pwd[usrname] == passwd)
+//     {
+//         cout << "Login success!" << endl;
+//         play();
+//     }
+//     else
+//     {
+//         cout << "Wrong password! Try again" << endl;
+//         prompt();
+//     }
+// }
+
+// void signup()
+// {
+//     string usrname, passwd1, passwd2;
+//     int lt;
+//     cout << "Username: ";
+//     cin >> usrname;
+//     if (usr_pwd.find(usrname) != usr_pwd.end())
+//     {
+//         cout << "Username already exist. press 1 to login or press 2 to try again..." << endl;
+//         cin >> lt;
+//         lt == 1 ? login() : signup();
+//     }
+//     cout << "Password: ";
+//     cin >> passwd1;
+//     cout << "Confirm password: ";
+//     cin >> passwd2;
+//     if (passwd1 == passwd2)
+//     {
+//         file.open(filename, ios::app);
+//         file << usrname << endl
+//              << passwd1 << endl;
+//         file.close();
+//         cout << "Registered successfully!" << endl;
+//         play();
+//     }
+//     else
+//     {
+//         cout << "Password doesn't match! Try again..." << endl;
+//         signup();
+//     }
+// }
+
+// void prompt()
+// {
+//     int ch;
+//     cout << "Enter your choice (1-login 2-register):";
+//     cin >> ch;
+//     switch (ch)
+//     {
+//     case 1:
+//         login();
+//         break;
+//     case 2:
+//         signup();
+//         break;
+//     default:
+//         cout << "Invalid Input. Try again..." << endl;
+//         prompt();
+//         break;
+//     }
+// }
