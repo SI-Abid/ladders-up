a45c73bb447b59bfda0a8c71eb5b095dda01a45f
SI-Abid <siaaid5323@gmai.com>
2021-06-14 23:38:29 +0600

    commit via vscode

--- main.cpp
+++ main.cpp
@@ -5,17 +5,9 @@
 #include "time.h"
 #include "library.h"
 #include "saiham.h"
-#include "tangin.h"
 
 using namespace std;
 
-int score = 0;
-// const char *name = "userdata.txt";
-const string title = "<~>-<~>-<~>-<~>-<~>-<~>-<~>-<~> LADDERS UP <~>-<~>-<~>-<~>-<~>-<~>-<~>-<~>";
-const string menu = "1: Play\n2: Highscore\n3: Log out\nEnter your option: ";
-
-void play();
-void Menu();
 
 int main()
 {
@@ -30,106 +22,3 @@ int main()
     return 0;
 }
 
-void Menu()
-{
-    CLEAR;
-    cout << menu;
-    string opt;
-    cin >> opt;
-    switch (opt[0])
-    {
-    case '1':
-        play();
-        break;
-    case '2':
-        Highscore();
-        break;
-    case '3':
-        return;
-
-    default:
-        break;
-    }
-
-    saveScore(score);
-    Menu();
-}
-
-void play()
-{
-    srand(time(0));
-    int n;
-    n = getDifficulty();
-    loadTraps(n);
-    Board gameboard(n);
-    gameboard.makeBoard();
-    int **board = gameboard.data;
-
-    int pos = 1, inc;
-
-    while (pos < n * n)
-    {
-
-        // system("cls");       // for windows
-
-        CLEAR; // for linux
-        cout << title << endl;
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int k = 0; k <= 4 * n; k++)
-            {
-                printf("-");
-            }
-            cout << endl;
-
-            for (int j = 0; j < n; j++)
-            {
-                if (pos == board[i][j])
-                {
-                    cout << "| X ";
-                }
-                else
-                {
-                    cout << "|   ";
-                }
-            }
-            cout << "|" << endl;
-        }
-        for (int k = 0; k <= 4 * n; k++)
-        {
-            cout << "-";
-        }
-        cout << endl;
-
-        if (isTrap[pos])
-        {
-            cout << "Oops! You have landed on a trap..." << endl;
-            score -= 5;
-        }
-        else if (pos % 19 == 0)
-        {
-            score += popQuiz();
-        }
-        else
-        {
-            score += 5;
-        }
-        int move = 1;
-
-        do
-        {
-            if (move > 10 || move < 1)
-            {
-                cout << "Invalid move. Try again..." << endl;
-            }
-            cout << "Enter how many cell you want to move forward(1-10): ";
-            cin >> move;
-        } while (move > 10 || move < 1);
-
-        pos += move;
-
-        // printf("\033");
-    }
-    cout << endl;
-}
