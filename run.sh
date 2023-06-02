83dfb29dcea0ed354696af78845c55c4f269cd05
Saiham Islam Abid <33659593+SI-Abid@users.noreply.github.com>
2023-06-02 14:13:42 +0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 read -p "Enter full screen mode and press Enter to play the game. Enjoy :)"
-g++ -std=c++14 main.cpp -o main.b && ./main.b && exit
+g++ -std=c++14 main.cpp -o main.b && ./main.b && rm main.b && exit
